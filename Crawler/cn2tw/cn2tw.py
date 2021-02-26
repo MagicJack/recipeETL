bb682293bbee995b877789867d00b53d2291703d
MagicJackTing <jackting@hotmail.com>
2021-02-26 19:44:30 +0800

    修正錯誤
    1. 加入 '类' 及 '水' 的轉換.
    2. 修正 '荀' 未轉換的錯誤.

--- Crawler/cn2tw/cn2tw.py
+++ Crawler/cn2tw/cn2tw.py
@@ -1,8 +1,8 @@
 import sys
 
-cnChar = list("仼优兰刴发咸国圣块坚塩头姜嫰岛庄徳择掦无柠榄殻温湿炼猕现瑶盐筛籮緑红绿苹荀荞葱蓝蔴蕯虾记调辢过选酱针铃铝铺锦门韮须饺马鱼鲍鲜鳯鸡麦黄黒龙")
-# 你 should change '姜'->'薑', '籮'->'蘿' manually. if run convertZ.exe to generate twChar
-twChar = list("任優蘭剁發鹹國聖塊堅鹽頭薑嫩島莊德擇揚無檸欖殼溫濕煉獼現瑤鹽篩蘿綠紅綠蘋荀蕎蔥藍麻薩蝦記調辣過選醬針鈴鋁鋪錦門韭須餃馬魚鮑鮮鳳雞麥黃黑龍")
+cnChar = list("仼优兰刴发咸国圣块坚塩头姜嫰岛庄徳择掦无柠榄殻温湿炼猕现瑶盐筛籮类緑红绿苹荀荞葱蓝蔴蕯虾记调辢过选酱针铃铝铺锦门韮须饺马鱼鲍鲜鳯鸡麦黄黒龙⽔")
+# 你 should change '姜'->'薑', '籮'->'蘿', '荀'->'筍' manually. and add '⽔' -> '水' manually. if run convertZ.exe to generate twChar
+twChar = list("任優蘭剁發鹹國聖塊堅鹽頭薑嫩島莊德擇揚無檸欖殼溫濕煉獼現瑤鹽篩蘿類綠紅綠蘋筍蕎蔥藍麻薩蝦記調辣過選醬針鈴鋁鋪錦門韭須餃馬魚鮑鮮鳳雞麥黃黑龍水")
 
 cn2twWord = {
     '豆乾': '豆干',
